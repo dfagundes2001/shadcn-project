04609e4485073b29f5867f02e016fa92ca08f0c0
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-03-03 09:50:57 -0300

    criar repositório

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Signin } from './screens/signin'
+
+import "./global.css"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Signin/>
+  </React.StrictMode>,
+)
---
+++ src/screens/signin.jsx
@@ -0,0 +1,7 @@
+import { Button } from '@/components/ui/button'
+
+export function Signin() {
+  return (
+    <Button>Signin</Button>
+  )
+}
